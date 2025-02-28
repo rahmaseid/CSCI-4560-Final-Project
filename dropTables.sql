fd9859eda51230c76abff6bfa5fe6e2302bf0ade
Rahma Seid <84355185+rahmaseid@users.noreply.github.com>
2025-02-28 17:10:50 -0600

    Create dropTables.sql

---
+++ dropTables.sql
@@ -0,0 +1,9 @@
+-- DROP TABLES in reverse order of table creation due to how contraints are figured
+
+DROP TABLE IF EXISTS bill_reminders;
+DROP TABLE IF EXISTS Bills;
+DROP TABLE IF EXISTS Transactions;
+DROP TABLE IF EXISTS Savings_goals;
+DROP TABLE IF EXISTS Categories;
+DROP TABLE IF EXISTS Income;
+DROP TABLE IF EXISTS Users;
